0166ba98fd8097aa008e1cc986616f2622c0195f
Your Name <you@example.com>
2026-07-10 10:31:40 +0800

    Remove duplicate Guestbook model

--- home/models.py
+++ home/models.py
@@ -31,10 +31,6 @@ class HomeContent(models.Model):
     
 
 
-class Guestbook(models.Model):
-    name = models.CharField(max_length=100)
-    message = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
